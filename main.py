2b7def4131167553f437803bad3c13d3bc9c025d
Daliana-Solis <dsolisso@uci.edu>
2023-09-25 18:49:37 -0500

    Added top nav bar, added column with headers

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from PyQt6.QtWidgets import QApplication, QGridLayout, QLabel, QWidget,\
+    QLineEdit, QPushButton, QMainWindow, QTableWidget
+
+from PyQt6.QtGui import QAction
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Student Management System")
+
+        #Top nav bar
+        file_menu_item = self.menuBar().addMenu("&File")
+        help_menu_item = self.menuBar().addMenu("&Help")
+
+        #Sub items
+        add_student_action = QAction("Add Student",self)
+        file_menu_item.addAction(add_student_action)
+
+        about_action = QAction("About", self)
+        help_menu_item.addAction(about_action)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+        #Table
+        self.table = QTableWidget()
+        self.table.setColumnCount(4) #4 columns in our table
+        self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.setCentralWidget(self.table) #Central widget
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def load_data(self):
+        self.table
+
+
+
+
+app = QApplication(sys.argv)
+student_mng = MainWindow()
+student_mng.show()
+sys.exit(app.exec())
