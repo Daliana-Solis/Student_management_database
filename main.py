9b9b128e6f588df7ea141c320f05e37a95031c8c
Daliana-Solis <dsolisso@uci.edu>
2023-09-25 19:05:28 -0500

    Added database information into the table

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import sys
-
+import sqlite3
 from PyQt6.QtWidgets import QApplication, QGridLayout, QLabel, QWidget,\
-    QLineEdit, QPushButton, QMainWindow, QTableWidget
-
+    QLineEdit, QPushButton, QMainWindow, QTableWidget, QTableWidgetItem
 from PyQt6.QtGui import QAction
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -26,12 +26,23 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4) #4 columns in our table
         self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False) #Hide vertical numbers for each row
         self.setCentralWidget(self.table) #Central widget
 
 
 
     def load_data(self):
-        self.table
+        #Connect with sql database
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students") #extract from DB
+        self.table.setRowCount(0) #Resets table, loads data as fresh
+
+        for row_num, row_data in enumerate(result):
+            self.table.insertRow(row_num)
+            for colmn_num, data in enumerate(row_data):
+                self.table.setItem(row_num, colmn_num, QTableWidgetItem(str(data)))
+        connection.close()
+
 
 
 
@@ -39,4 +50,5 @@ class MainWindow(QMainWindow):
 app = QApplication(sys.argv)
 student_mng = MainWindow()
 student_mng.show()
+student_mng.load_data()
 sys.exit(app.exec())
